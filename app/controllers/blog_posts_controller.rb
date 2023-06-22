6e9fcf4206e7ef0e66b41a9223fec57afc4c0c49
jorndejager <jorndejager35@gmail.com>
2023-06-22 21:59:52 +0200

    Revert "commit"
    This reverts commit 9495dd1c29cf0320160422dd49589945d3dacbad.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,51 +1,7 @@
 class BlogPostsController < ApplicationController
-    before_action :authenticate_user!, except:[:index, :show, :like, :unlike, :debug ]
-    before_action :set_blog_post, except: [:index, :new, :create, :debug]
-
-
-    def like
-    @blog_post = BlogPost.find(params[:id])
-
-    if session[:liked_blog_posts]&.include?(@blog_post.id)
-      flash[:alert] = 'You have already liked this blog post.'
-    else
-      @blog_post.increment!(:likes)
-      session[:liked_blog_posts] ||= []
-      session[:liked_blog_posts] << @blog_post.id
-
-      flash[:notice] = 'Blog post liked.'
-    end
-
-    redirect_to @blog_post
-  end
-
-  def unlike
-    @blog_post = BlogPost.find(params[:id])
-
-    if session[:liked_blog_posts]&.include?(@blog_post.id)
-      @blog_post.decrement!(:likes)
-      session[:liked_blog_posts].delete(@blog_post.id)
-
-      flash[:notice] = 'Blog post unliked.'
-    else
-      flash[:alert] = 'You have not liked this blog post.'
-    end
-
-    redirect_to @blog_post
-  end
-
-
-    #def like
-     #   @blog_post.increment!(:likes)
-      #  redirect_to @blog_post
-   # end
-
-   # def unlike
-    #    @blog_post.decrement!(:likes)
-     #   redirect_to @blog_post
-    #end
-
-
+    before_action :authenticate_user!, except:[:index, :show ]
+    before_action :set_blog_post, except: [:index, :new, :create]
+    
 
     def index
         @blog_posts = BlogPost.all
@@ -57,8 +13,8 @@ class BlogPostsController < ApplicationController
     def new
         @blog_post= BlogPost.new
     end
-
-    def create
+        
+    def create 
         @blog_post = BlogPost.new(blog_post_params)
         if @blog_post.save
             redirect_to @blog_post
@@ -76,7 +32,7 @@ class BlogPostsController < ApplicationController
             redirect_to @blog_post
         else
             render :edit, status: :unprocessable_entity
-
+            
         end
     end
 
@@ -88,7 +44,6 @@ end
 
 
     private
-
 
     def blog_post_params
         params.require( :blog_post).permit(:title, :body)
--- db/migrate/20230613134347_add_like_to_post.rb
+++
@@ -1,7 +0,0 @@
-class AddLikeToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_column :likes, :integer, default: 0
-
-
-  end
-end
